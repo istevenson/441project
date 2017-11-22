b5069345d05b582546932cdefd1f14db6c914e53
Stevenson <istevenson@dstsystems.com>
2017-11-22 15:22:49 -0600

    string to vector

--- game/src/game/core.clj
+++ game/src/game/core.clj
@@ -1,6 +1,8 @@
 (ns game.core
   (:gen-class))
 
+(use '[clojure.string :only (split triml)])
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
@@ -8,10 +10,15 @@
   (do
   	(println "Enter the name of the file (with the extension)...")
   	(let [fileName (read-line)]
-  		(println (str "Opening '" fileName "'..."))
+  		(println (str "Opening '" fileName "'..." \newline))
   		;; File path for debbugging purposes
   		;;(println(System/getProperty "user.dir"))
 
   		;; Open file and store contents in var currentFile
-  		(def currentFile (slurp (str "src/game/files/"fileName))))
-  		(println currentFile)))
+  		(def currentFile (slurp (str "src/game/files/"fileName)))
+  		(println (str "The file '" fileName "' contains: " \newline currentFile))
+
+  		;; Start working with file contents
+  		
+  		(println (split currentFile #"\s+"))
+  		)))
