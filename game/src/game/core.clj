4262066a53f0650ef8a375da1c7e7bf354cbaedb
Stevenson <istevenson@dstsystems.com>
2017-11-28 17:52:30 -0600

    game play

--- game/src/game/core.clj
+++ game/src/game/core.clj
@@ -2,6 +2,7 @@
   (:gen-class))
 
 (use '[clojure.string :only (split triml)])
+(require '[clojure.math.numeric-tower :as math])
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -18,7 +19,29 @@
   		(def currentFile (slurp (str "src/game/files/"fileName)))
   		(println (str "The file '" fileName "' contains: " \newline currentFile))
 
-  		;; Start working with file contents
-  		
+  		;;;; Start working with file contents
   		(println (split currentFile #"\s+"))
-  		)))
+  		;; Get number of coins in game play - 0th index in list of coins
+  		(def coinList (split currentFile #"\s+"))
+  		(def numCoins (nth coinList 0))
+  		(println (str "..........Simulating Game Play.........."))
+  		(println (str "There are " numCoins " coins in this game."))
+  		(println (str "If both players play with the optimal strategy..."))
+  		(println (str "Player 1:" (math/abs -2) "points"))
+  		(println (str "Player 2: points"))
+
+  		(defn PickCoin []
+  			(def optA (first coinList))
+  			(def optB (last coinList))
+  			(def choice(if (< 10 8) "optB" "optA"))
+  			(println choice)
+  		)
+
+  		(PickCoin)
+	)
+))
+
+
+
+
+
